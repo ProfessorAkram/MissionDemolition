5ec745125ecbecc54836c6ce817b6787614c8c8c
Professor Akram <professorakramburris@gmail.com>
2022-02-14 13:13:34 -0600

    Eased and Limited Camera
    Added Lerp easing to camera
    Set camera limits
    Set orthographicSize of camera

--- MissionDemolition-Unity/Assets/Scripts/FollowCam.cs
+++ MissionDemolition-Unity/Assets/Scripts/FollowCam.cs
@@ -16,6 +16,10 @@ public class FollowCam : MonoBehaviour
     /**** VARIABLES ****/
     static public GameObject POI; //the static point of interest
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f; //ease of the camera motion
+    public Vector2 miniumXY = Vector2.zero; 
+
     [Header("Set Dynamically")]
     public float camZ; //Desired Z posiiton of the camera
 
@@ -30,10 +34,17 @@ public class FollowCam : MonoBehaviour
     {
         if (POI == null) return; //if there is no POI exit method
 
-        
         Vector3 destination = POI.transform.position;//Get the positition of the POI
+
+        //Limit the camera's minium XY values
+        destination.x = Mathf.Max(miniumXY.x, destination.x); 
+        destination.y = Mathf.Max(miniumXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing); //interpolate from the current camera position towards the destination
         destination.z = camZ; //reset the z of the destination to the camera z
         transform.position = destination; //Set position of the camera to the destination
+
+        Camera.main.orthographicSize = destination.y + 10; //Sets the orthographicSize of the camera to keep the ground in view
         
     }//end FixedUpdate()
 }
